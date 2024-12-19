19f252a185ac78752b5284eb82936585b9008d12
Valentin Calomino <vcalomino@fi.uba.ar>
2024-12-19 16:00:03 -0300

    feat: agrego endpoint para crear producto

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+app.post("/products/create", (req, res) => {
+  res.send("Hola");
+});
+
 app.listen(port, () => {
   console.log(`Servidor escuchando en http://localhost:${port}`);
 });
