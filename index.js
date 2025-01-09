3170d5338ecf19adda872ded4dcddd1e1e7cd03e
guada <grojas@fi.uba.ar>
2025-01-09 17:53:48 -0300

    Agrego import del router de ordenes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const productRouter = require("./routes/productRouter.js");
 const addressRouter = require("./routes/addressRouter.js");
+const orderRouter = require("./routes/orderRouter.js")
 const app = express();
 const port = 3000;
 app.use(express.json());
