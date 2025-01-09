6f4d64749ddbdcc800c685cf66aa7283e4655ffb
guada <grojas@fi.uba.ar>
2025-01-09 18:30:36 -0300

    fix: Conecto productos de la orden con productos de base de datos

--- repository/orderRepository.js
+++ repository/orderRepository.js
@@ -6,14 +6,39 @@ const orderRepository = {
     const order = await prisma.order.create({
       data: {
         totalPrice: orderData.totalPrice,
-        address: orderData.address,
-        addressId: orderData.addressId,
-        products: orderData.productse,
+        address: {
+          connect: { id: orderData.addressId }
+        },
         ownerName: orderData.ownerName,
-        orderStatus: orderData.orderStatus
+        orderStatus: orderData.orderStatus,
+        products: {
+          create: orderData.products.map(product => ({
+            product: {
+              connect: { id: product.productId }
+            },
+            quantity: product.quantity
+          }))
+        }
       },
+      include: {
+        products: {
+          include: {
+            product: true
+          }
+        }
+      }
     });
-    return order;
+    const formattedOrder = {
+      ...order,
+      products: order.products.map(orderProduct => ({
+        productId: orderProduct.product.id,
+        name: orderProduct.product.name,
+        quantity: orderProduct.quantity,
+        value: orderProduct.product.value
+      }))
+    };
+
+    return formattedOrder;
   },
 };
 
