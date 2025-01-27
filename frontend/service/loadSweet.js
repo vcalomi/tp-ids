1a1f08df948f4287a588030855ca10678b0f43cf
Valentin Calomino <vcalomino@fi.uba.ar>
2025-01-27 18:43:16 -0300

    feat: filter sweet products

---
+++ frontend/service/loadSweet.js
@@ -0,0 +1,36 @@
+function loadSweet() {
+  const products = JSON.parse(localStorage.getItem("products"));
+  const title = document.getElementById("TITLE");
+  const sweetProducts = products.filter((product) =>
+    product.type.includes("DULCE")
+  );
+
+  for (let i = 0; i < sweetProducts.length; i++) {
+    const sweetDiv = document.createElement("div");
+    sweetDiv.className = "menu-dulces";
+    const productImg = document.createElement("img");
+    productImg.src =
+      "https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D";
+    productImg.className = "food-image";
+    productImg.id = "avocado-toast";
+    const productDiv = document.createElement("div");
+    productDiv.className = "producto";
+    const productName = document.createElement("p");
+    productName.className = "nombre";
+    productName.innerText = sweetProducts[i].name;
+    const productDescription = document.createElement("p");
+    productDescription.className = "descripcion";
+    productDescription.innerText = sweetProducts[i].description;
+    const productPrice = document.createElement("p");
+    productPrice.className = "precio";
+    productPrice.innerText = sweetProducts[i].value;
+    productDiv.appendChild(productName);
+    productDiv.appendChild(productDescription);
+    productDiv.appendChild(productPrice);
+    title.append(sweetDiv);
+    sweetDiv.appendChild(productImg);
+    sweetDiv.appendChild(productDiv);
+  }
+}
+
+loadSweet();
