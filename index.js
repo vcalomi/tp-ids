fff88e65f326a79696c271b6d0426f0a3818eeb8
Valentin Calomino <vcalomino@fi.uba.ar>
2025-02-06 16:59:51 -0300

    Update index.js

--- index.js
+++ index.js
@@ -19,15 +19,6 @@ app.use(
   })
 );
 
-
-app.get("/products", (req, res) => {
-  res.json([{ id: 1, name: "Producto de prueba" }]);
-});
-
-app.listen(3000, () => {
-  console.log("Servidor corriendo en el puerto 3000");
-});
-
 app.use("/uploads", express.static(path.join(__dirname, "public", "uploads")));
 
 app.use("/auth", authRouter);
