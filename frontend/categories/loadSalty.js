cdd386348ec29acc6b596b87649daf9c4fbf73e8
Guadalupe Rojas <grojas@fi.uba.ar>
2025-02-06 10:50:41 -0300

    feat: servir las imagenes desde el backend y cargarlas en el frontend

--- frontend/categories/loadSalty.js
+++ frontend/categories/loadSalty.js
@@ -11,8 +11,7 @@ function loadSalty() {
     const saltyDiv = document.createElement("div");
     saltyDiv.className = "menu-dulces";
     const productImg = document.createElement("img");
-    productImg.src =
-      "https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D";
+    productImg.src = `http://localhost:3000${saltyProducts[i].image}`;
     productImg.className = "food-image";
     productImg.id = "avocado-toast";
     const productDiv = document.createElement("div");
