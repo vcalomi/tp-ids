b42c00de78c0b4925ae42fda10bab4618d3a274b
vcalomino <vcalomino@fi.uba.ar>
2025-02-07 14:44:43 -0300

    fix: agrego el header donde se envia el token a los permitidos por cors

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(
   cors({
     origin: process.env.REQUESTS_ORIGIN,
     methods: "GET,POST,PUT,DELETE,OPTIONS",
-    allowedHeaders: "Content-Type,Authorization,x-auth-header",
+    allowedHeaders: "Content-Type,Authorization,x-auth-token",
     credentials: true,
   })
 );
