46a02b480ae85382a5a05b59002940ee047d5458
guada <grojas@fi.uba.ar>
2025-02-18 19:39:57 -0300

    Agrego alerta por usuario existente

--- frontend/authentication/sign-up.js
+++ frontend/authentication/sign-up.js
@@ -3,31 +3,30 @@ import { API_URL } from "../config.js";
 
 document
   .getElementById("registerForm")
-  .addEventListener("submit", function (event) {
+  .addEventListener("submit", async function (event) {
     event.preventDefault();
 
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
 
-    fetch(`${API_URL}/auth/register`, {
+    const response = await fetch(`${API_URL}/auth/register`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({ username, password }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.token) {
-          const token = data.token;
-          const decodedToken = getPayloadFromToken(token);
-          const user = decodedToken.user;
-          localStorage.setItem("token", token);
-          localStorage.setItem("user", JSON.stringify(user));
-          window.location.href = "../index.html";
-        }
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
+    });
+    if (response.status === 400) {
+      alert("Usuario existente!");
+      return;
+    }
+    const data = await response.json();
+    if (data.token) {
+      const token = data.token;
+      const decodedToken = getPayloadFromToken(token);
+      const user = decodedToken.user;
+      localStorage.setItem("token", token);
+      localStorage.setItem("user", JSON.stringify(user));
+      window.location.href = "../index.html";
+    }
   });
